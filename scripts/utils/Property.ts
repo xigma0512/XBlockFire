b085ac9f96ccd0c1367e5a8c03065b95794eb58f
xigma0512 <xig1517@gmail.com>
2025-06-01 23:37:49 +0800

    feat: 新增 Utils

---
+++ scripts/utils/Property.ts
@@ -0,0 +1,65 @@
+import { Entity, ItemStack, world } from "@minecraft/server"
+
+type WorldPropertyList = {
+}
+
+type EntityDynamicPropertyList = {
+    'id': string
+}
+
+type ScoreBoardPropertyList = {
+}
+
+type EntityNativePropertyList = { 
+}
+
+type WorldPropertyId = keyof WorldPropertyList;
+type EntityDynamicPropertyId = keyof EntityDynamicPropertyList;
+type ScoreBoardPropertyId = keyof ScoreBoardPropertyList;
+type EntityNativePropertyId = keyof EntityNativePropertyList;
+
+function world_property<T extends WorldPropertyId>(name: T) {
+    return world.getDynamicProperty(name) as WorldPropertyList[T];
+}
+
+function set_world_property<T extends WorldPropertyId>(name: T, value: WorldPropertyList[T]) {
+    world.setDynamicProperty(name, value);
+}
+
+function entity_dynamic_property<T extends EntityDynamicPropertyId>(entity: Entity | ItemStack, name: T) {
+    return entity.getDynamicProperty(name) as EntityDynamicPropertyList[T];
+}
+
+function set_entity_dynamic_property<T extends EntityDynamicPropertyId>(entity: Entity | ItemStack, name: T, value: EntityDynamicPropertyList[T]) {
+    entity.setDynamicProperty(name, value);
+}
+
+function scoreboard_property<T extends ScoreBoardPropertyId>(entity: Entity, name: T) {
+    if (world.scoreboard.getObjective(name) === undefined) world.scoreboard.addObjective(name);
+
+    const scoreboard = world.scoreboard.getObjective(name);
+    try { return scoreboard!.getScore(entity) as ScoreBoardPropertyList[T]; }
+    catch { return 0; }
+}
+
+function set_scoreboard_property<T extends ScoreBoardPropertyId>(entity: Entity, name: T, value: ScoreBoardPropertyList[T]) {
+    if (world.scoreboard.getObjective(name) === undefined) world.scoreboard.addObjective(name);
+
+    const scoreboard = world.scoreboard.getObjective(name);
+    scoreboard!.setScore(entity, Number(value));
+}
+
+function entity_native_property<T extends EntityNativePropertyId>(entity: Entity, name: T) {
+    return entity.getProperty(name) as EntityNativePropertyList[T];
+}
+
+function set_entity_native_property<T extends EntityNativePropertyId>(entity: Entity, name: T, value: EntityNativePropertyList[T]) {
+    entity.setProperty(name, value);
+}
+
+export { 
+    world_property, set_world_property, 
+    entity_dynamic_property, set_entity_dynamic_property, 
+    scoreboard_property, set_scoreboard_property,
+    entity_native_property, set_entity_native_property
+};
