7c3be832a1697fec647d0c9b064dd0e53a7f989c
xigma0512 <xig1517@gmail.com>
2025-06-02 19:24:35 +0800

    feat: 新增 MagazineComponent

--- scripts/modules/gun_system/components/Component.ts
+++ scripts/modules/gun_system/components/Component.ts
@@ -1,11 +1,13 @@
 import { DamageComponent } from "./DamageComponent";
 import { GunComponent } from "./GunComponent";
 import { ItemComponent } from "./ItemComponent";
+import { MagazineComponent } from "./MagazineComponent";
 
 export type ComponentTypes = {
     'damage': DamageComponent;
     'gun': GunComponent;
     'item': ItemComponent;
+    'magazine': MagazineComponent;
 }
 
 export class Component {
