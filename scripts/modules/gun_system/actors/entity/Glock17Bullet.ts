205056f9ec750d77b26e6bdb7c29f7799b410416
xigma0512 <xig1517@gmail.com>
2025-06-03 14:39:25 +0800

    feat: 為 Glock17, Glock17Bullet 新增 Components

--- scripts/modules/gun_system/actors/entity/Glock17Bullet.ts
+++ scripts/modules/gun_system/actors/entity/Glock17Bullet.ts
@@ -1,6 +1,9 @@
-import { Entity } from "@minecraft/server";
-import { DamageComponent } from "../../components/DamageComponent";
 import { EntityActor } from "../Actor";
+import { DamageComponent } from "../../components/DamageComponent";
+import { BulletComponent } from "../../components/BulletComponent";
+import { EntityComponent } from "../../components/EntityComponent";
+
+import { Entity, VanillaEntityIdentifier } from "@minecraft/server";
 
 export class Glock17Bullet extends EntityActor {
 
@@ -8,11 +11,22 @@ export class Glock17Bullet extends EntityActor {
         super('glock17bullet', entity);
         
         this.components
+            .set('entity', new EntityComponent({
+                entityTypeId: 'xblockfire:bullet' as VanillaEntityIdentifier,
+                spawnOptions: {
+                    spawnEvent: 'glock17bullet'
+                }
+            }))
+            .set('bullet', new BulletComponent({
+                flightSpeed: 1000
+            }))
             .set('damage', new DamageComponent({
                 near: { head: 10, body: 5, legs: 3 },
                 medium: { head: 8, body: 4, legs: 2 },
                 far: { head: 6, body: 3, legs: 1 }
             }));
+        
+        this.setEntity();
     }
 
 }
