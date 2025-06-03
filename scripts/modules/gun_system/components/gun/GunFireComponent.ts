05710096b0fe3c56aaf88c7c85ec3d0ffd6176ba
xigma0512 <xig1517@gmail.com>
2025-06-03 14:31:30 +0800

    refactor: GunComponent 碎片化，並移除舊的槍支組件

---
+++ scripts/modules/gun_system/components/gun/GunFireComponent.ts
@@ -0,0 +1,26 @@
+// types/Enums.ts (保持不變)
+import { FireModeEnum } from "../../types/Enums";
+import { Component } from "../Component";
+
+type FiringDataType = {
+    fire_mode: FireModeEnum;
+    release_to_fire: boolean;
+    bullet_spread: number;
+    fire_rate: number;
+}
+
+export class GunFireComponent extends Component {
+    readonly fire_mode: FireModeEnum;
+    readonly release_to_fire: boolean;
+    readonly bullet_spread: number;
+    readonly fire_rate: number;
+
+    constructor(data: FiringDataType) {
+        super('gun_fire');
+        
+        this.fire_mode = data.fire_mode;
+        this.release_to_fire = data.release_to_fire;
+        this.bullet_spread = data.bullet_spread;
+        this.fire_rate = data.fire_rate;
+    }
+}
