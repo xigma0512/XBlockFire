c2d7ad33a3ac151b9fbd04b12944f30c25fe9421
xigma0512 <xig1517@gmail.com>
2025-06-03 19:23:38 +0800

    feat: 為 Flashbang 和 SmokeGrenade Actor 新增 GrenadeComponent

--- scripts/modules/gun_system/actors/entity/SmokeGrenade.ts
+++ scripts/modules/gun_system/actors/entity/SmokeGrenade.ts
@@ -3,16 +3,25 @@ import { ProjectileReboundComponent } from "../../components/ProjectileReboundCo
 import { entity_native_property } from "../../../../utils/Property";
 
 import { Entity } from "@minecraft/server";
+import { GrenadeComponent } from "../../components/GrenadeComponent";
+import { GrenadeTypeEnum } from "../../types/Enums";
 
 export class SmokeGrenade extends EntityActor {
 
     constructor(entity: Entity) {
         super('smoke_grenade', entity);
         
+        const throwingType = entity_native_property(entity, 'grenade:throwing_type');
         this.components
             .set('projectile_rebound', new ProjectileReboundComponent(
                 {
-                    bounceFactor: (entity_native_property(entity, 'grenade:throwing_type') === 0 ? 0.6 : 0.2)
+                    bounceFactor: (throwingType === 0 ? 0.6 : 0.2)
+                }
+            ))
+            .set('grenade', new GrenadeComponent(
+                {
+                    grenadeTypeId: GrenadeTypeEnum.Flashbang,
+                    executeDelay: (throwingType === 0 ? 70 : 40)
                 }
             ));
     }
