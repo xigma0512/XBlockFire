b0d1988983cd50b580e2447c4ba875803716a9e2
xigma0512 <xig1517@gmail.com>
2025-06-09 18:11:28 +0800

    feat: `MapRegister`

---
+++ scripts/modules/room/systems/map/MapRegister.ts
@@ -0,0 +1,28 @@
+import GameMap from "./GameMap";
+
+export class MapRegister {
+    
+    private static _instance: MapRegister;
+    static get instance() { return (this._instance || (this._instance = new this())); }
+    
+    readonly availableMaps: Map<string, GameMap>;
+    
+    constructor() {
+        this.availableMaps = new Map();
+        this.initializeMaps();
+    }
+
+    getMap(mapId: string) {
+        if (this.availableMaps.has(mapId)) {
+            return this.availableMaps.get(mapId) as GameMap;
+        }
+        throw Error(`Cannot get map ${mapId}.`);
+    }
+
+    private initializeMaps() {
+        for (const [mapId, mapInfo] of Object.entries(GameMap)) {
+            this.availableMaps.set(mapId, mapInfo);
+        }
+    }
+
+}
---
+++ scripts/modules/room/types/GameMap.d.ts
@@ -0,0 +1,20 @@
+type TeamTagEnum = number;
+
+interface Position {
+    x: number;
+    y: number;
+    z: number;
+}
+
+type PositionType = {
+    'attacker_spawns';
+    'defender_spawns';
+    'bomb_targets';
+}
+
+declare interface GameMap {
+    id: number;
+    name: string;
+    description: string;
+    positions: Record<PositionType, Position | Position[]>;
+}
