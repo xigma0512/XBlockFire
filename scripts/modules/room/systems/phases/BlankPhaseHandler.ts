29cf7535bf0efabf4444b10d5b341c07fbb69314
xigma0512 <xig1517@gmail.com>
2025-06-09 22:08:46 +0800

    feat: 新增 PhaseManager 和 BlankPhase 類別，實現階段管理功能

---
+++ scripts/modules/room/systems/phases/BlankPhaseHandler.ts
@@ -0,0 +1,32 @@
+import { GameModeEnum } from "../../types/Enum";
+import { GameRoomManager } from "../GameRoom";
+
+import { BombPlant_IdlePhase } from "./bomb_plant/Idle";
+
+export class BlankPhase implements IPhaseHandler {
+
+    constructor(private readonly roomId: string) { }
+
+    on_entry() {
+        console.warn('Entry blank phase.');
+    }
+
+    on_running() {
+        this.transitions();
+    }
+
+    on_exit() {
+        console.warn('Exit blank phase.');
+    }
+
+    private transitions() {
+        const room = GameRoomManager.instance.getRoom(this.roomId);
+
+        switch(room.gameMode) {
+            case GameModeEnum.BombPlant: 
+                room.phaseManager.updatePhase(new BombPlant_IdlePhase(this.roomId));
+                break;
+        }
+    }
+
+}
