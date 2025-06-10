e91112576d75edf9166e34397c16cbc624959cb2
xigma0512 <xig1517@gmail.com>
2025-06-10 15:20:12 +0800

    feat: 新增炸彈模式的狀態機雛形

---
+++ scripts/modules/room/systems/phases/bomb_plant/Buying.ts
@@ -0,0 +1,95 @@
+import { GameRoomManager } from "../../GameRoom";
+import { BombPlant_ActionPhase } from "./Action";
+import { MapRegister } from "../../map/MapRegister";
+
+import { TeamTagEnum } from "../../../../weapon/types/Enums";
+import { entity_dynamic_property, set_entity_dynamic_property } from "../../../../../utils/Property";
+import { Broadcast } from "../../../../../utils/Broadcast";
+
+import { GameMode, InputPermissionCategory } from "@minecraft/server";
+import { Vector3Utils } from "@minecraft/math";
+
+const COUNTDOWN_TIME = 30 * 20;
+
+export class BombPlant_BuyingPhase implements IPhaseHandler {
+
+    private currentTick: number = COUNTDOWN_TIME;
+
+    constructor(private readonly roomId: number) { }
+
+    on_entry() {
+        this.currentTick = COUNTDOWN_TIME;
+        spawnPlayers(this.roomId);
+        resetPlayers(this.roomId);
+
+        console.warn('Entry BombPlant:buying phase.');
+    }
+
+    on_running() {
+        const room = GameRoomManager.instance.getRoom(this.roomId);
+        const members = room.memberManager.getPlayers();
+
+        let actionbarText = [
+            `Buying phase will end in ${(this.currentTick / 20).toFixed(0)} seconds.\n`, 
+            `Right-click the feather to open the shop.`
+        ];
+        
+        Broadcast.actionbar(actionbarText, members);
+        this.currentTick --;
+
+        this.transitions();
+    }
+
+    on_exit() {
+        const room = GameRoomManager.instance.getRoom(this.roomId);
+        const member = room.memberManager;
+
+        for (const player of member.getPlayers()) {
+            player.inputPermissions.setPermissionCategory(InputPermissionCategory.LateralMovement, true);
+        }
+        
+        console.warn('Exit BombPlant:buying phase.');
+    }
+
+    private transitions() {
+        const room = GameRoomManager.instance.getRoom(this.roomId);
+        if (this.currentTick <= 0) room.phaseManager.updatePhase(new BombPlant_ActionPhase(this.roomId));
+    }
+
+}
+
+function spawnPlayers(roomId: number) {
+    const room = GameRoomManager.instance.getRoom(roomId);
+    const member = room.memberManager;
+    const gameMap = MapRegister.instance.getMap(room.gameMapId);
+
+    const attackerSpawns = gameMap.positions.attacker_spawns;
+    const defenderSpawns = gameMap.positions.defender_spawns;
+
+    const spawns = {
+        [TeamTagEnum.Attacker]: attackerSpawns,
+        [TeamTagEnum.Defender]: defenderSpawns,
+    }
+
+    let nextSpawnIndex = new Array<number>(10).fill(0);
+
+    for (const player of member.getPlayers()) {
+        const playerTeam = entity_dynamic_property(player, 'player:team');
+        if (playerTeam === TeamTagEnum.Spectator) continue;
+
+        const playerTeamSpawns = spawns[playerTeam];
+        const spawnIndex = nextSpawnIndex[playerTeam]++ % playerTeamSpawns.length;
+        
+        player.teleport(Vector3Utils.add(playerTeamSpawns[spawnIndex], {x: 0.5, z: 0.5}));
+        player.inputPermissions.setPermissionCategory(InputPermissionCategory.LateralMovement, false);
+    }
+}
+
+function resetPlayers(roomId: number) {
+    const room = GameRoomManager.instance.getRoom(roomId);
+    const players = room.memberManager.getPlayers();
+    for (const player of players) {
+        set_entity_dynamic_property(player, 'player:is_alive', true);
+        player.setGameMode(GameMode.adventure);
+    }
+}
---
+++ scripts/modules/room/systems/phases/bomb_plant/RoundEnd.ts
@@ -0,0 +1,91 @@
+import { GameRoomManager } from "../../GameRoom";
+import { BombPlant_BuyingPhase } from "./Buying";
+import { BombPlant_GameOverPhase } from "./Gameover";
+
+import { TeamTagEnum } from "../../../../weapon/types/Enums";
+import { ColorTable, ColorType } from "../../../../../utils/Color";
+import { entity_dynamic_property } from "../../../../../utils/Property";
+import { set_variable, variable } from "../../../../../utils/Variable";
+import { Broadcast } from "../../../../../utils/Broadcast";
+
+const INCOME = [4000, 2000];
+const WINNING_SCORE = 7;
+const COUNTDOWN_TIME = 10 * 20;
+
+export class BombPlant_RoundEndPhase implements IPhaseHandler {
+
+    private currentTick: number = COUNTDOWN_TIME;
+
+    constructor(private readonly roomId: number) { }
+
+    on_entry() {
+        this.currentTick = COUNTDOWN_TIME;
+
+        const room = GameRoomManager.instance.getRoom(this.roomId);
+        const member = room.memberManager;
+        const economy = room.economyManager;
+
+        const winnerTeam = variable(`${this.roomId}.round_winner`) as TeamTagEnum;
+        if (winnerTeam === TeamTagEnum.Attacker) {
+            set_variable(`${this.roomId}.attacker_score`, (variable(`${this.roomId}.attacker_score`) ?? 0) + 1);
+        } else if (winnerTeam === TeamTagEnum.Defender) { 
+            set_variable(`${this.roomId}.defender_score`, (variable(`${this.roomId}.defender_score`) ?? 0) + 1);
+        }
+
+        for (const player of member.getPlayers()) {
+            const playerTeam = entity_dynamic_property(player, 'player:team');
+            const earn = INCOME[(playerTeam === winnerTeam) ? 0 : 1];
+            economy.addMoney(player, earn);
+            player.sendMessage(`${ColorTable[ColorType.Gray]}Round Income: +${earn}`);
+        }
+
+        console.warn('Entry BombPlant:roundEnd phase.');
+    }
+
+    on_running() {
+        const room = GameRoomManager.instance.getRoom(this.roomId);
+        const members = room.memberManager.getPlayers();
+        
+        const actionbarText = `${ColorTable[ColorType.Yellow]}Next round start in ${(this.currentTick / 20).toFixed(0)} seconds.`;
+        Broadcast.actionbar(actionbarText, members);
+        
+        this.currentTick --;
+        this.transitions();
+    }
+
+    on_exit() {
+        console.warn('Exit BombPlant:roundEnd phase.');
+    }
+
+    private transitions() {
+        const room = GameRoomManager.instance.getRoom(this.roomId);
+        const phase = room.phaseManager;
+
+        const attackerScore = variable(`${this.roomId}.attacker_score`);
+        const defenderScore = variable(`${this.roomId}.defender_score`);
+
+        let winner = null;
+        if (attackerScore >= WINNING_SCORE) winner = TeamTagEnum.Attacker;
+        if (defenderScore >= WINNING_SCORE) winner = TeamTagEnum.Defender;
+
+        if (winner) {
+            set_variable(`${this.roomId}.winner`, winner);
+            phase.updatePhase(new BombPlant_GameOverPhase(this.roomId));
+            return;
+        }
+
+        if (this.currentTick <= 0) {
+            
+            if (attackerScore + defenderScore >= WINNING_SCORE) {
+                switchSide();
+            }
+            
+            phase.updatePhase(new BombPlant_BuyingPhase(this.roomId));
+        }
+    }
+
+}
+
+function switchSide() {
+    console.warn('switching side!!');
+}
