f3edc7c09bce570c952c226433be7d209256ab94
xigma0512 <xig1517@gmail.com>
2025-06-23 15:16:20 +0800

    refactor: 移除不必要的狀態表，直接使用靜態狀態字串

--- scripts/modules/gameroom/systems/phase/bomb_plant/PhaseEnum.ts
+++ scripts/modules/gameroom/systems/phase/bomb_plant/PhaseEnum.ts
@@ -6,12 +6,3 @@ export const enum BP_PhaseEnum {
     Gameover,
     BombPlanted
 }
-
-export const BP_PhaseEnumTable = {
-    [BP_PhaseEnum.Idle]: "Waiting",
-    [BP_PhaseEnum.Buying]: "Buying",
-    [BP_PhaseEnum.Action]: "Action",
-    [BP_PhaseEnum.RoundEnd]: "Round End",
-    [BP_PhaseEnum.Gameover]: "Gameover",
-    [BP_PhaseEnum.BombPlanted]: "Bomb has been planted",
-}
