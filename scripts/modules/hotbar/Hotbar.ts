3f45650dc744970a35572596462d885bcd35feca
xigma0512 <xig1517@gmail.com>
2025-06-27 23:13:28 +0800

    feat: Hotbar & HotbarManager

---
+++ scripts/modules/hotbar/Hotbar.ts
@@ -0,0 +1,59 @@
+import { ItemLockMode, ItemStack, Player } from "@minecraft/server";
+
+export class Hotbar {
+    
+    private readonly items: Array<ItemStack | undefined>;
+
+    constructor() {
+        this.items = new Array<ItemStack | undefined>(9);
+    }
+
+    clearAll() {
+        this.items.fill(undefined);
+    }
+
+    set(index: number, item: ItemStack | undefined) {
+        if (item) {
+            item.lockMode = ItemLockMode.slot;
+        }
+        this.items[index] = item;
+        return this;
+    }
+
+    at(index: number) {
+        return this.items.at(index);
+    }
+}
+
+export class HotbarManager {
+
+    private static _instance: HotbarManager;
+    static get instance() { return (this._instance || (this._instance = new this())); }
+    
+    private players = new Map<Player, Hotbar>();
+
+    private constructor() { 
+        this.players = new Map();
+    }
+
+    getHotbar(target: Player) {
+        if (!this.players.has(target)) this.players.set(target, new Hotbar());
+        return this.players.get(target)!;
+    }
+
+    updateHotbar(target: Player) {
+        const container = target.getComponent('inventory')!.container;
+        const hotbar = this.getHotbar(target);
+        for (let index = 0; index < 10; index ++) {
+            hotbar.set(index, container.getItem(index));
+        }
+    }
+
+    sendHotbar(target: Player) {
+        const container = target.getComponent('inventory')!.container;
+        const hotbar = this.getHotbar(target);
+        for (let index = 0; index < 10; index ++) {
+            container.setItem(index, hotbar.at(index));
+        }
+    }
+}
