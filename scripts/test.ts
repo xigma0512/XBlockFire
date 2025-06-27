6443e6a1eaf0c3f147c3296c3ca6e2e44dfbd61d
xigma0512 <xig1517@gmail.com>
2025-06-27 23:13:28 +0800

    feat: Hotbar & HotbarManager

--- scripts/test.ts
+++ scripts/test.ts
@@ -2,12 +2,24 @@ import { world } from "@minecraft/server";
 import { Glock17 } from "./modules/weapon/actors/item/Glock17";
 import { set_entity_dynamic_property } from "./utils/Property";
 import { TeamEnum } from "./modules/gameroom/types/TeamEnum";
+import { HotbarManager } from "./modules/hotbar/Hotbar";
 
+let hotbar: any;
 world.afterEvents.chatSend.subscribe(ev => {
     const sender = ev.sender;
     
-    const glock17 = new Glock17();
-    sender.getComponent('inventory')?.container.addItem(glock17.item);
+    // sender.getComponent('inventory')?.container.addItem(glock17.item);
+    
+    if (ev.message === 'test') {
+        hotbar = HotbarManager.instance.getHotbar(sender);
+        const glock17 = new Glock17();
+        hotbar.setItem(0, glock17.item);
+        hotbar.setItem(4, glock17.item);
+        hotbar.setItem(7, glock17.item);
+    }
+    if (ev.message === 'send') {
+        hotbar.send();
+    }
 
     if (ev.message === '1') {
         for(const player of world.getAllPlayers()) {
