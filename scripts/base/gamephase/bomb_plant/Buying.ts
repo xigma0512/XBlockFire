3a72aab2100f8a8380f2ba2ea909cffb32216ebb
xigma0512 <xig1517@gmail.com>
2025-06-29 22:22:22 +0800

    refactor: 重構 Action、Buying 和 RoundEnd 階段，簡化邏輯並改善可讀性

--- scripts/base/gamephase/bomb_plant/Buying.ts
+++ scripts/base/gamephase/bomb_plant/Buying.ts
@@ -28,8 +28,6 @@ export class BuyingPhase implements IPhaseHandler {
     on_entry() {
         this._currentTick = config.COUNTDOWN_TIME;
         spawnPlayers(this.roomId);
-        sendHotbar(this.roomId);
-        resetPlayers(this.roomId);
 
         console.warn(`[Room ${this.roomId}] Entry BP:buying phase.`);
     }
@@ -41,15 +39,7 @@ export class BuyingPhase implements IPhaseHandler {
     }
 
     on_exit() {
-        const room = GameRoomManager.instance.getRoom(this.roomId);
-        const member = room.memberManager;
-
-        for (const player of member.getPlayers()) {
-            player.inputPermissions.setPermissionCategory(InputPermissionCategory.LateralMovement, true);
-            HotbarManager.getHotbar(player).set(8, undefined);
-            HotbarManager.sendHotbar(player);
-        }
-        
+        resetPlayer(this.roomId);        
         console.warn(`[Room ${this.roomId}] Exit BP:buying phase.`);
     }
 
@@ -76,40 +66,40 @@ function spawnPlayers(roomId: number) {
     }
 
     for (const player of member.getPlayers()) {
-        const playerTeam = entity_dynamic_property(player, 'player:team');
         if (entity_dynamic_property(player, 'player:is_spectator')) continue;
 
+        // teleport
+        const playerTeam = entity_dynamic_property(player, 'player:team');
         const playerTeamSpawns = spawns[playerTeam];
         const spawnIndex = nextSpawnIndex[playerTeam]++ % playerTeamSpawns.length;
-        
         player.teleport(playerTeamSpawns[spawnIndex]);
-
+        
+        // player initial setting
         player.inputPermissions.setPermissionCategory(InputPermissionCategory.LateralMovement, false);
-        HotbarManager.getHotbar(player).set(8, new ItemStack('minecraft:feather'));
-        HotbarManager.sendHotbar(player);
-    }
-}
-
-function sendHotbar(roomId: number) {
-    const room = GameRoomManager.instance.getRoom(roomId);
-    const member = room.memberManager;
-
-    for (const player of member.getPlayers()) {
+        set_entity_dynamic_property(player, 'player:is_alive', true);
+        player.setGameMode(GameMode.Adventure);
+    
+        // send hotbar
         HotbarManager.getHotbar(player).set(8, new ItemStack('minecraft:feather'));
         HotbarManager.sendHotbar(player);
     }
 
+    // send c4
     const attackers = member.getPlayers({ team: TeamEnum.Attacker });
     const bombPlayer = attackers[Math.floor(Math.random() * attackers.length)];
     HotbarManager.getHotbar(bombPlayer).set(3, new ItemStack('xblockfire:c4'));
     HotbarManager.sendHotbar(bombPlayer);
 }
 
-function resetPlayers(roomId: number) {
+function resetPlayer(roomId: number) {
     const room = GameRoomManager.instance.getRoom(roomId);
-    const players = room.memberManager.getPlayers();
-    for (const player of players) {
-        set_entity_dynamic_property(player, 'player:is_alive', true);
-        player.setGameMode(GameMode.Adventure);
+    const member = room.memberManager;
+
+    for (const player of member.getPlayers()) {
+        player.inputPermissions.setPermissionCategory(InputPermissionCategory.LateralMovement, true);
+
+        // clear feather(shop)
+        HotbarManager.getHotbar(player).set(8, undefined);
+        HotbarManager.sendHotbar(player);
     }
 }
