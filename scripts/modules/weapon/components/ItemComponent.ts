5286ea6021702c13562a3a28ef86f0fda74ccd79
xigma0512 <xig1517@gmail.com>
2025-07-01 14:12:29 +0800

    refactor: 將 ItemComponent 的屬性改為可選

--- scripts/modules/weapon/components/ItemComponent.ts
+++ scripts/modules/weapon/components/ItemComponent.ts
@@ -2,7 +2,7 @@ import { Component } from "./Component";
 
 import { ItemLockMode } from "@minecraft/server";
 
-type ComponentDataType = {
+type ComponentDataType = Partial<{
     nametag: string;
     lore: string[];
 
@@ -10,25 +10,25 @@ type ComponentDataType = {
     lockMode: ItemLockMode;
     canDestroy: string[];
     canPlaceOn: string[];
-}
+}>
 
 export class ItemComponent extends Component {
 
-    readonly nametag: string;
-    readonly lore: string[];
+    readonly nametag?: string;
+    readonly lore?: string[];
 
     readonly keepOnDeath: boolean;
     readonly lockMode: ItemLockMode;
-    readonly canDestroy: string[];
-    readonly canPlaceOn: string[];
+    readonly canDestroy?: string[];
+    readonly canPlaceOn?: string[];
 
     constructor(data: ComponentDataType) {
         super('item');
 
         this.nametag = data.nametag;
         this.lore = data.lore;
-        this.keepOnDeath = data.keepOnDeath;
-        this.lockMode = data.lockMode;
+        this.keepOnDeath = data.keepOnDeath ?? false;
+        this.lockMode = data.lockMode ?? ItemLockMode.none;
         this.canDestroy = data.canDestroy;
         this.canPlaceOn = data.canPlaceOn;
     }
