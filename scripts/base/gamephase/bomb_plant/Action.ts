3e301101fc3edcb8a06b45be381a37a83f872a47
xigma0512 <xig1517@gmail.com>
2025-07-01 13:12:39 +0800

    fix: 炸彈被安裝後，防守方全部陣亡不會提前結束回合。並且重構了一些寫法

--- scripts/base/gamephase/bomb_plant/Action.ts
+++ scripts/base/gamephase/bomb_plant/Action.ts
@@ -13,6 +13,57 @@ import { set_variable } from "../../../utils/Variable";
 
 const config = Config.action;
 
+const enum EndReasonEnum {
+    'Time-up' = 1,
+    'Attacker-Eliminated',
+    'Attacker-Disconnect',
+    'Defender-Eliminated',
+    'Defender-Disconnect'
+};
+
+const endReasonTable = {
+    [EndReasonEnum['Time-up']]: {
+        winner: TeamEnum.Defender,
+        message: [
+            `${FC.Yellow}Time Up. This Round Is Over.\n`,
+            `${FC.Yellow}Defenders Win.\n`
+        ],
+        nextPhaseGenerator: (roomId: number) => new RoundEndPhase(roomId)
+    },
+    [EndReasonEnum['Attacker-Eliminated']]: {
+        winner: TeamEnum.Defender,
+        message: [
+            `${FC.Yellow}Attackers Have Been Eliminated.\n`,
+            `${FC.Yellow}Defenders Win.\n`
+        ],
+        nextPhaseGenerator: (roomId: number) => new RoundEndPhase(roomId)
+    },
+    [EndReasonEnum['Attacker-Disconnect']]: {
+        winner: TeamEnum.Defender,
+        message: [
+            `${FC.Yellow}Attackers Have Been Eliminated.\n`,
+            `${FC.Yellow}Defenders Win.\n`
+        ],
+        nextPhaseGenerator: (roomId: number) => new GameOverPhase(roomId)
+    },
+    [EndReasonEnum['Defender-Eliminated']]: {
+        winner: TeamEnum.Attacker,
+        message: [
+            `${FC.Yellow}Defenders Have Been Eliminated.\n`,
+            `${FC.Yellow}Attackers Win.\n`
+        ],
+        nextPhaseGenerator: (roomId: number) => new RoundEndPhase(roomId)
+    },
+    [EndReasonEnum['Defender-Disconnect']]: {
+        winner: TeamEnum.Attacker,
+        message: [
+            `${FC.Yellow}Defenders Have Been Eliminated.\n`,
+            `${FC.Yellow}Attackers Win.\n`
+        ],
+        nextPhaseGenerator: (roomId: number) => new GameOverPhase(roomId)
+    }
+}
+
 export class ActionPhase implements IPhaseHandler {
 
     readonly phaseTag = BombPlantPhaseEnum.Action;
@@ -44,14 +95,6 @@ export class ActionPhase implements IPhaseHandler {
         const member = room.memberManager;
         const phase = room.phaseManager;
 
-        const enum EndReasonEnum {
-            'Time-up' = 1,
-            'Attacker-Eliminated',
-            'Attacker-Disconnect',
-            'Defender-Eliminated',
-            'Defender-Disconnect'
-        }
-
         let endReason: EndReasonEnum | null = null;
 
         const attackers = member.getPlayers({ team: TeamEnum.Attacker });
@@ -59,49 +102,21 @@ export class ActionPhase implements IPhaseHandler {
 
         const attackersAlive = member.getPlayers({ team: TeamEnum.Attacker, is_alive: true });
         const defendersAlive = member.getPlayers({ team: TeamEnum.Defender, is_alive: true });
-        
+
         if (attackersAlive.length === 0) endReason = EndReasonEnum['Attacker-Eliminated'];
         if (defendersAlive.length === 0) endReason = EndReasonEnum['Defender-Eliminated'];
-
         if (attackers.length === 0) endReason = EndReasonEnum['Attacker-Disconnect'];
         if (defenders.length === 0) endReason = EndReasonEnum['Defender-Disconnect'];
-
         if (this.currentTick <= 0) endReason = EndReasonEnum['Time-up'];
     
         if (endReason) {
-
             const separator = `${FC.White}---------------\n`;
-            let message = [separator];
-            let nextPhase: IPhaseHandler = new RoundEndPhase(this.roomId);
-    
-            let winner = TeamEnum.Defender;
-            switch (endReason) {
-                case EndReasonEnum['Time-up']:
-                    message.push(`${FC.Yellow}Time Up. This Round Is Over.\n`);
-                    message.push(`${FC.Yellow}Defenders Win\n`);
-                    winner = TeamEnum.Defender;
-                    break;
+            const result = endReasonTable[endReason];
 
-                case EndReasonEnum['Attacker-Disconnect']:
-                    nextPhase = new GameOverPhase(this.roomId);
-                case EndReasonEnum['Attacker-Eliminated']:
-                    message.push(`${FC.Yellow}Defenders Win\n`);
-                    winner = TeamEnum.Defender;
-                    break;
+            Broadcast.message([separator, ...result.message, separator]);
 
-                case EndReasonEnum['Defender-Disconnect']:
-                    nextPhase = new GameOverPhase(this.roomId);
-                case EndReasonEnum['Defender-Eliminated']:
-                    message.push(`${FC.Yellow}Attackers Win\n`);
-                    winner = TeamEnum.Attacker;
-                    break;
-            }
-
-            message.push(separator);
-            Broadcast.message(message);
-
-            set_variable(`${this.roomId}.round_winner`, winner);
-            phase.updatePhase(nextPhase);
+            set_variable(`${this.roomId}.round_winner`, result.winner);
+            phase.updatePhase(result.nextPhaseGenerator(this.roomId));
         }
     }
 
