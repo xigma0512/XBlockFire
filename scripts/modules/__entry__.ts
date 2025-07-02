73a499d8d42248398a20f4d7924391e124db4745
xigma0512 <xig1517@gmail.com>
2025-07-03 02:58:35 +0800

    feat: 新增 WeaponInfo，手持槍枝顯示子彈在 actionbar

--- scripts/modules/__entry__.ts
+++ scripts/modules/__entry__.ts
@@ -4,3 +4,5 @@ import './weapon/systems/gun/GunFireSystem'
 import './weapon/systems/gun/GunReloadSystem'
 import './weapon/systems/bullet/BulletSystem'
 import './weapon/systems/grenade/GrenadeSystem'
+
+import './hud/weapon/WeaponInfo'
