195b86009e60b49982b997e1e5aea6ef77e41eec
xigma0512 <xig1517@gmail.com>
2025-07-02 18:38:23 +0800

    feat: 新增 `player:is_holding_gun` 屬性

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,4 +1,21 @@
-import { world } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 
 world.afterEvents.chatSend.subscribe(ev => {
+})
+
+world.afterEvents.worldLoad.subscribe(() => {
+    system.runInterval(() => {
+        const players = world.getPlayers({
+            propertyOptions: [
+                {
+                    propertyId: 'player:is_holding_gun',
+                    value: true
+                }
+            ]
+        });
+        
+        for (const player of players) {
+            player.onScreenDisplay.setActionBar('123')
+        }
+    })
 })
