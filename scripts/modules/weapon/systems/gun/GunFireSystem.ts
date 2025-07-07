e592a09043ec5f66c293684173e2a2f5f92d3df5
xigma0512 <xig1517@gmail.com>
2025-07-07 21:50:09 +0800

    fix: 連續點可能導致無法停止開槍

--- scripts/modules/weapon/systems/gun/GunFireSystem.ts
+++ scripts/modules/weapon/systems/gun/GunFireSystem.ts
@@ -9,26 +9,15 @@ import { getPlayerHandItem } from "../../../../utils/others/Entity";
 import { entity_native_property } from "../../../../utils/Property";
 
 import { Player, system, world } from "@minecraft/server";
-import { ItemStopUseAfterEvent, PlayerHotbarSelectedSlotChangeAfterEvent, PlayerLeaveAfterEvent } from "@minecraft/server";
-
-interface StopFiringListeners {
-    afterItemStopUse: (ev: ItemStopUseAfterEvent) => void;
-    afterPlayerHotbarSelected: (ev: PlayerHotbarSelectedSlotChangeAfterEvent) => void;
-    afterPlayerLeave: (ev: PlayerLeaveAfterEvent) => void;
-}
 
 class GunFireSystem {
 
     private static _instance: GunFireSystem;
     static get instance() { return (this._instance || (this._instance = new this())); }
 
-    private _firingTask: Map<Player, number>;
-    private _stopFiringCallback: Map<Player, StopFiringListeners>;
     private _cooldowns: Set<Player>;
 
     private constructor() {
-        this._firingTask = new Map();
-        this._stopFiringCallback = new Map();
         this._cooldowns = new Set();
     }
 
@@ -46,45 +35,20 @@ class GunFireSystem {
                 this.semiAutoFire(player, gunActor);
                 break;
         }
-
-        this._stopFiringCallback.set(player, {
-            afterItemStopUse: world.afterEvents.itemStopUse.subscribe((ev) => { 
-                if (ev.source.id === player.id) this.stopFiringTrigger(player); 
-            }),
-            afterPlayerHotbarSelected: world.afterEvents.playerHotbarSelectedSlotChange.subscribe((ev) => {
-                if (ev.player.id === player.id) this.stopFiringTrigger(player); 
-            }),
-            afterPlayerLeave: world.afterEvents.playerLeave.subscribe((ev) => {
-                if (ev.playerId === player.id) this.stopFiringTrigger(player); 
-            })
-        });
-    }
-
-    private stopFiringTrigger(player: Player) {
-        if (this._firingTask.has(player)) {
-            const taskId = this._firingTask.get(player)!;
-            system.clearRun(taskId);
-            this._firingTask.delete(player);
-        }
-
-        if (this._stopFiringCallback.has(player)) {
-            const callbacks = this._stopFiringCallback.get(player)!;
-            
-            world.afterEvents.itemStopUse.unsubscribe(callbacks.afterItemStopUse);
-            world.afterEvents.playerHotbarSelectedSlotChange.unsubscribe(callbacks.afterPlayerHotbarSelected);
-            world.afterEvents.playerLeave.unsubscribe(callbacks.afterPlayerLeave);
-
-            this._stopFiringCallback.delete(player);   
-        }
     }
 
     private fullAutoFire(player: Player, actor: ItemActor) {
+        if (this._cooldowns.has(player)) return;
+
         const gunFireComp = actor.getComponent('gun_fire')!;
         const fireRate = gunFireComp.fire_rate;
 
         this.fire(player, actor);
         const taskId = system.runInterval(() => this.fire(player, actor), fireRate);
-        this._firingTask.set(player, taskId);
+        this._cooldowns.add(player);
+        system.runTimeout(() => this._cooldowns.delete(player), fireRate);
+        
+        this.stopFiringTrigger(player, taskId);
     }
 
     private semiAutoFire(player: Player, actor: ItemActor) {
@@ -124,6 +88,27 @@ class GunFireSystem {
         
         for (let _ = 0; _ < gunFireComp.bullet_spread; _ ++) {
             BulletSystem.instance.spawnBullet(player, gunComp.gunTypeId, shootOffset);
+        }
+    }
+
+    private stopFiringTrigger(player: Player, firingTaskId: number) {
+
+        const afterItemStopUse = world.afterEvents.itemStopUse.subscribe((ev) => { 
+            if (ev.source.id === player.id) stopFire(); 
+        });
+        const afterPlayerHotbarSelected = world.afterEvents.playerHotbarSelectedSlotChange.subscribe((ev) => {
+            if (ev.player.id === player.id) stopFire(); 
+        });
+        const afterPlayerLeave = world.afterEvents.playerLeave.subscribe((ev) => {
+            if (ev.playerId === player.id) stopFire(); 
+        });
+
+        const stopFire = () => {
+            system.clearRun(firingTaskId);
+
+            world.afterEvents.itemStopUse.unsubscribe(afterItemStopUse);
+            world.afterEvents.playerHotbarSelectedSlotChange.unsubscribe(afterPlayerHotbarSelected);
+            world.afterEvents.playerLeave.unsubscribe(afterPlayerLeave);
         }
     }
 }
