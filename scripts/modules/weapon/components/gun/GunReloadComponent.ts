9762c0d58b73e7046f8c79c2db74eb72903d94fa
xigma0512 <xig1517@gmail.com>
2025-07-08 20:06:20 +0800

    feat: 在 GunReloadComponent 新增裝子彈音效的選項

--- scripts/modules/weapon/components/gun/GunReloadComponent.ts
+++ scripts/modules/weapon/components/gun/GunReloadComponent.ts
@@ -2,15 +2,19 @@ import { Component } from "../Component";
 
 type ComponentDataType = {
     reload_time: number;
+    reload_sound?: string;
 }
 
 export class GunReloadComponent extends Component {
     
     readonly reload_time: number;
+    
+    readonly reload_sound?: string;
 
     constructor(data: ComponentDataType) {
         super('gun_reload');
         
         this.reload_time = data.reload_time;
+        this.reload_sound = data.reload_sound;
     }
 }
