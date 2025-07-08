e5c75be622982a2d36167b52d773908d625cea80
xigma0512 <xig1517@gmail.com>
2025-07-08 23:09:06 +0800

    feat: 新增 M4A4 槍枝互動

---
+++ scripts/modules/weapon/actors/entity/bullet/M4A4Bullet.ts
@@ -0,0 +1,29 @@
+import { DummyEntity, EntityActor } from "../../Actor";
+import { BulletComponent } from "../../../components/bullet/BulletComponent";
+import { BulletDamageComponent } from "../../../components/bullet/BulletDamageComponent";
+import { EntityComponent } from "../../../components/EntityComponent";
+
+import { VanillaEntityIdentifier } from "@minecraft/server";
+
+export class M4A4Bullet extends EntityActor {
+
+    constructor(entity: DummyEntity) {
+        super('m4a4bullet', entity);
+        
+        this.components
+            .set('entity', new EntityComponent({
+                entityTypeId: 'xblockfire:bullet' as VanillaEntityIdentifier
+            }))
+            .set('bullet', new BulletComponent({
+                flightSpeed: 1000
+            }))
+            .set('bullet_damage', new BulletDamageComponent({
+                near: { head: 16, body: 8, legs: 6 },
+                medium: { head: 12, body: 6, legs: 5 },
+                far: { head: 7, body: 4, legs: 3 } 
+            }));
+        
+        this.setEntity();
+    }
+
+}
