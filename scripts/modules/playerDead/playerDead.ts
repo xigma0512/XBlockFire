bf25e59eb33f0da192e5566b16aee0870896c07c
xigma0512 <xig1517@gmail.com>
2025-07-09 22:41:15 +0800

    feat: 玩家死亡信息

--- scripts/modules/playerDead/playerDead.ts
+++ scripts/modules/playerDead/playerDead.ts
@@ -3,7 +3,12 @@ import { GameRoomManager } from "../../base/gameroom/GameRoom";
 import { MemberManager } from "../../base/gameroom/member/MemberManager";
 import { gameEvents } from "../../event/EventEmitter";
 
-import { set_entity_dynamic_property } from "../../utils/Property";
+import { TeamEnum } from "../../types/TeamEnum";
+
+import { Broadcast } from "../../utils/Broadcast";
+import { FormatCode as FC } from "../../utils/FormatCode";
+import { entity_dynamic_property, set_entity_dynamic_property } from "../../utils/Property";
+
 import { GameMode, ItemStack, Player, system, world } from "@minecraft/server";
 
 world.afterEvents.entityDie.subscribe(ev => {
@@ -16,14 +21,13 @@ world.afterEvents.entityDie.subscribe(ev => {
 
 gameEvents.subscribe('playerDied', (ev) => {
     if (!MemberManager.isInRoom(ev.deadPlayer)) return;
-
     const deadPlayer = ev.deadPlayer;
-    
     dropC4(deadPlayer);
     set_entity_dynamic_property(deadPlayer, 'player:is_alive', false);
     deadPlayer.getComponent('inventory')?.container.clearAll();
     deadPlayer.setGameMode(GameMode.Spectator);
 
+    if (ev.attacker && ev.attacker instanceof Player) showDeathMessage(deadPlayer, ev.attacker);
 });
 
 
@@ -36,4 +40,19 @@ function dropC4(player: Player) {
     const room = GameRoomManager.instance.getRoom(roomId);
     const bombManager = room.bombManager;
     bombManager.updateState(new BombDroppedState(roomId, player.location));
+}
+
+function showDeathMessage(deadPlayer: Player, attacker: Player) {
+    const roomId = MemberManager.getPlayerRoomId(deadPlayer)!;
+    const room = GameRoomManager.instance.getRoom(roomId);
+
+    const deadPlayerTeam = entity_dynamic_property(deadPlayer, 'player:team');
+    const attackerTeam = entity_dynamic_property(attacker, 'player:team');
+
+    const playerTeamStr = (team: TeamEnum, name: string) => (team === TeamEnum.Attacker) ? `${FC.Red}[A]${name}` : `${FC.Aqua}[D]${name}`;
+    
+    Broadcast.message(
+        `${FC.Bold}${playerTeamStr(attackerTeam, attacker.name)} ${FC.DarkRed}eliminated ${playerTeamStr(deadPlayerTeam, deadPlayer.name)}`,
+        room.memberManager.getPlayers()
+    );
 }
