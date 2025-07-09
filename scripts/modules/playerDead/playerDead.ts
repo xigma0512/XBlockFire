ce34659dc5fd7cae3042371b77a497328365660c
xigma0512 <xig1517@gmail.com>
2025-07-09 21:28:52 +0800

    feat: 持包者死亡掉包

--- scripts/modules/playerDead/playerDead.ts
+++ scripts/modules/playerDead/playerDead.ts
@@ -1,8 +1,10 @@
+import { BombDroppedState } from "../../base/bombstate/states/Dropped";
+import { GameRoomManager } from "../../base/gameroom/GameRoom";
 import { MemberManager } from "../../base/gameroom/member/MemberManager";
 import { gameEvents } from "../../event/EventEmitter";
 
 import { set_entity_dynamic_property } from "../../utils/Property";
-import { GameMode, Player, system, world } from "@minecraft/server";
+import { GameMode, ItemStack, Player, system, world } from "@minecraft/server";
 
 world.afterEvents.entityDie.subscribe(ev => {
     if (!(ev.deadEntity instanceof Player) || !MemberManager.isInRoom(ev.deadEntity)) return;
@@ -16,7 +18,22 @@ gameEvents.subscribe('playerDied', (ev) => {
     if (!MemberManager.isInRoom(ev.deadPlayer)) return;
 
     const deadPlayer = ev.deadPlayer;
+    
+    dropC4(deadPlayer);
     set_entity_dynamic_property(deadPlayer, 'player:is_alive', false);
     deadPlayer.getComponent('inventory')?.container.clearAll();
     deadPlayer.setGameMode(GameMode.Spectator);
+
 });
+
+
+const C4_ITEM_ID = 'xblockfire:c4';
+function dropC4(player: Player) {
+    const container = player.getComponent('inventory')!.container!;
+    if (container.find(new ItemStack(C4_ITEM_ID)) === undefined) return;
+
+    const roomId = MemberManager.getPlayerRoomId(player)!;
+    const room = GameRoomManager.instance.getRoom(roomId);
+    const bombManager = room.bombManager;
+    bombManager.updateState(new BombDroppedState(roomId, player.location));
+}
