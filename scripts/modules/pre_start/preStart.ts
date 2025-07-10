0ea9df7e21f037bc9a815a76c11e257ec92c95f3
xigma0512 <xig1517@gmail.com>
2025-07-10 16:05:45 +0800

    feat: 玩家加入遊戲時，隱藏特定的 hud

--- scripts/modules/pre_start/preStart.ts
+++ scripts/modules/pre_start/preStart.ts
@@ -1,4 +1,4 @@
-import { world } from "@minecraft/server";
+import { HudElement, HudVisibility, world } from "@minecraft/server";
 
 world.afterEvents.worldLoad.subscribe(() => {
     world.gameRules.keepInventory = true;
@@ -6,4 +6,13 @@ world.afterEvents.worldLoad.subscribe(() => {
     world.gameRules.fallDamage = false;
     world.gameRules.pvp = true;
     world.gameRules.showDeathMessages = false;
+});
+
+world.afterEvents.playerSpawn.subscribe(ev => {
+    if (ev.initialSpawn) {
+        ev.player.onScreenDisplay.setHudVisibility(HudVisibility.Hide, [
+            HudElement.ItemText,
+            HudElement.Armor
+        ]);
+    }
 });
