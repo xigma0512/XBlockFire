c114f83e0e17dc861bd88cac3c61e69db67e4c3a
xigma0512 <xig1517@gmail.com>
2025-07-13 13:53:05 +0800

    test: 調整測試內容

--- scripts/test.ts
+++ scripts/test.ts
@@ -5,12 +5,4 @@ import { M4A4 } from "./base/weapon/actors/item/M4A4";
 import { TeamEnum } from "./types/TeamEnum";
 
 world.afterEvents.chatSend.subscribe(ev => {
-    set_entity_native_property(ev.sender, 'player:can_use_item', true);
-    const ak = new AK47();
-    const m4 = new M4A4();
-    ev.sender.getComponent('inventory')?.container.addItem(ak.item);
-    ev.sender.getComponent('inventory')?.container.addItem(m4.item);
-    for (const player of world.getAllPlayers()) {
-        set_entity_dynamic_property(player, 'player:team', TeamEnum.Attacker);
-    }
 })
