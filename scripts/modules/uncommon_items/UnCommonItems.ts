17626d9ad2402e855ef448cb581fbd7bde92599d
xigma0512 <xig1517@gmail.com>
2025-07-20 18:24:59 +0800

    fix: 存放團隊裝備的箱子被卸載後導致無法讀取物品

--- scripts/modules/uncommon_items/UnCommonItems.ts
+++ scripts/modules/uncommon_items/UnCommonItems.ts
@@ -1,4 +1,4 @@
-import { ItemLockMode, world } from "@minecraft/server"
+import { Container, ItemLockMode, world } from "@minecraft/server"
 import { Config } from "../../settings/config";
 
 const CONTAINER_LOCATION = Config.uncommon_items.CONTAINER_LOCATION;
@@ -6,13 +6,17 @@ const CONTAINER_LOCATION = Config.uncommon_items.CONTAINER_LOCATION;
 const ITEM_LIST = Config.uncommon_items.ITEM_LIST;
 
 export class UnCommonItems {
+    private static container: Container | undefined;
     static getItem(type: keyof typeof ITEM_LIST) {
-        const chest = world.getDimension('overworld').getBlock(CONTAINER_LOCATION);
-        if (chest === undefined || chest.getComponent('inventory') === undefined) {
-            throw Error('Cannot found UnCommonItems container.');
-        } 
-        const container = chest.getComponent('inventory')!.container!;
-        const item = container.getItem(ITEM_LIST[type])?.clone();
+        if (this.container === undefined) {
+            const chest = world.getDimension('overworld').getBlock(CONTAINER_LOCATION);
+            if (chest === undefined || chest.getComponent('inventory') === undefined) {
+                throw Error('Cannot found UnCommonItems container.');
+            } 
+            this.container = chest.getComponent('inventory')!.container!;
+        }
+
+        const item = this.container.getItem(ITEM_LIST[type])?.clone();
         if (item === undefined) {
             throw Error(`${type} item is undefined`);
         }
