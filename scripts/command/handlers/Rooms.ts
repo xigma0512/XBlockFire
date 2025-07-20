0dd2864703a1e725820843e33e8f150ef04ace7c
xigma0512 <xig1517@gmail.com>
2025-07-20 16:59:39 +0800

    feat: 新增自選隊伍功能

--- scripts/command/handlers/Rooms.ts
+++ scripts/command/handlers/Rooms.ts
@@ -7,13 +7,16 @@ import { PreRoundStartPhase } from "../../base/gamephase/bomb_plant/PreRoundStar
 import { GameModeEnum } from "../../types/gameroom/GameModeEnum";
 
 import { FormatCode as FC } from "../../utils/FormatCode";
+import { set_entity_dynamic_property } from "../../utils/Property";
 
 import { Player } from "@minecraft/server";
+import { TeamEnum } from "../../types/TeamEnum";
+import { Broadcast } from "../../utils/Broadcast";
 
 function createRoom(executer: Player, ...args: string[]) {
     const [gamemode, mapId] = args;
 
-    if (!gamemode || !mapId) {
+    if (gamemode === undefined || mapId === undefined) {
         throw Error("Missing argument '<gamemode>' or '<map_id>'. Usage: /scriptevent blockfire:room create <gamemode> <mapId>");
     }
 
@@ -22,18 +25,18 @@ function createRoom(executer: Player, ...args: string[]) {
     }
 
     GameRoomFactory.createRoom(gamemode as GameModeEnum, Number(mapId));
-    executer.sendMessage(`${FC.Gray}>> ${FC.Yellow}Create room successfully.`);
+    executer.sendMessage(`${FC.Gray}>> ${FC.Yellow}Created room.`);
 }
 
 function playerJoin(executer: Player) {    
-    if (MemberManager.includePlayer(executer)) throw Error(`You have already in game.`);
+    if (MemberManager.includePlayer(executer)) throw Error(`You are already in the game.`);
 
     MemberManager.joinRoom(executer);
     executer.sendMessage(`${FC.Gray}>> ${FC.Green}You join the room `);
 }
 
 function playerLeave(executer: Player) {
-    if (MemberManager.includePlayer(executer)) throw Error(`You are not in game.`);
+    if (MemberManager.includePlayer(executer)) throw Error(`You are not in the game.`);
 
     MemberManager.leaveRoom(executer);
     executer.sendMessage(`${FC.Gray}>> ${FC.Red}You leave the room `);
@@ -47,6 +50,20 @@ function forceStart(executer: Player) {
     executer.sendMessage(`${FC.Gray}>> ${FC.LightPurple}Force start.`);
 }
 
+function selectTeam(executer: Player, ...args: string[]) {
+    const [team] = args;
+    if (team === undefined) {
+        throw Error("Missing argument '<team>'. Usage: /scriptevent blockfire:room select_team <team>");
+    }
+
+    if (!Object.values<string>(TeamEnum).includes(team)) {
+        throw Error(`Unknown mode ${team}.`);
+    }
+
+    set_entity_dynamic_property(executer, 'player:team', team as TeamEnum);
+    Broadcast.message(`${FC.Gold}${executer.name} join [${team}]`);
+}
+
 function roomCmd(executer: Player, ...args: string[]) {
     const cmdType = args[0];
     if (!cmdType) throw Error('Missing arguments <type>. Usage: /scriptevent blockfire:room <type> <...args>');
@@ -57,6 +74,7 @@ function roomCmd(executer: Player, ...args: string[]) {
         case 'join': playerJoin(executer); break;
         case 'leave': playerLeave(executer); break;
         case 'start': forceStart(executer); break;
+        case 'select_team': selectTeam(executer, ...args); break;
         default: 
             throw Error(`there is no command type as ${cmdType}`);
             break;
