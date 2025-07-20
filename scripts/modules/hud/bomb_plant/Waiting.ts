b28d4e0fced0740b1e069b027e3c6ccff72c773a
xigma0512 <xig1517@gmail.com>
2025-07-20 16:38:54 +0800

    feat: 新增等待期間隊伍顯示

--- scripts/modules/hud/bomb_plant/Waiting.ts
+++ scripts/modules/hud/bomb_plant/Waiting.ts
@@ -3,6 +3,8 @@ import { MemberManager } from "../../../base/member/MemberManager";
 import { PhaseManager } from "../../../base/gamephase/PhaseManager";
 import { MapRegister } from "../../../base/gamemap/MapRegister";
 
+import { TeamEnum } from "../../../types/TeamEnum";
+
 import { FormatCode as FC } from "../../../utils/FormatCode";
 import { Broadcast } from "../../../utils/Broadcast";
 
@@ -49,12 +51,17 @@ export class WaitingHud implements InGameHud {
         const map = MapRegister.getMap(gameroom().gameMapId);
         const playerCount = players.length;
 
+        const defenders = MemberManager.getPlayers({team: TeamEnum.Defender});
+        const attackers = MemberManager.getPlayers({team: TeamEnum.Attacker});
+
         const message = [
             `${FC.Bold}${FC.Yellow}  XBlockFire  `,
             ` ${FC.Gray}${todayStr}`,
             '',
             `Map: ${FC.Green}${map.name}`,
-            `Players: ${FC.Green}${playerCount}`,
+            `Players: ${FC.Green}${playerCount} ${FC.White}(${FC.Aqua}${defenders.length}${FC.White}/${FC.Red}${attackers.length}${FC.White})`,
+            ...defenders.map(p => `${FC.Gray}- ${FC.Aqua}${p.name}`),
+            ...attackers.map(p => `${FC.Gray}- ${FC.Red}${p.name}`),
             '',
             `Mode:`,
             `${FC.Green}${gameroom().gameMode}`,
