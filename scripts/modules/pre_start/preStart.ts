c7137bc5561f1a80c070ab72694a7ba762b88390
xigma0512 <xig1517@gmail.com>
2025-07-21 15:22:13 +0800

    feat: 修正遊戲規則設定

--- scripts/modules/pre_start/preStart.ts
+++ scripts/modules/pre_start/preStart.ts
@@ -1,4 +1,4 @@
-import { HudElement, HudVisibility, world } from "@minecraft/server";
+import { Difficulty, HudElement, HudVisibility, world } from "@minecraft/server";
 
 world.afterEvents.worldLoad.subscribe(() => {
     world.gameRules.keepInventory = true;
@@ -6,13 +6,17 @@ world.afterEvents.worldLoad.subscribe(() => {
     world.gameRules.fallDamage = false;
     world.gameRules.pvp = true;
     world.gameRules.showDeathMessages = false;
+    world.gameRules.doMobSpawning = false;
+    world.gameRules.doImmediateRespawn = true;
+    world.setDifficulty(Difficulty.Hard);
 });
 
 world.afterEvents.playerSpawn.subscribe(ev => {
     if (ev.initialSpawn) {
         ev.player.onScreenDisplay.setHudVisibility(HudVisibility.Hide, [
             HudElement.ItemText,
-            HudElement.Armor
+            HudElement.Armor,
+            HudElement.AirBubbles
         ]);
     }
 });
