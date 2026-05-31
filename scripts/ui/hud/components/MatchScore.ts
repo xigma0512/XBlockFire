b4bc18ff008cc432a61896f065b1206f93106080
xigma0512 <xig1517@gmail.com>
2026-06-01 06:55:46 +0800

    feat: 回合中當時間小於15秒時，改變顯示方式

--- scripts/ui/hud/components/MatchScore.ts
+++ scripts/ui/hud/components/MatchScore.ts
@@ -5,6 +5,6 @@ export class MatchScore {
      * Generates a string like "<Score> || <Time> || <Score>"
      */
     static format(leftScore: number, timeStr: string, rightScore: number): string {
-        return `${FC.MaterialEmerald}${leftScore} ${FC.Gray}|| ${FC.White}${timeStr} ${FC.Gray}|| ${FC.MaterialRedstone}${rightScore}`;
+        return `${FC.MaterialEmerald}${leftScore} ${FC.Gray}|| ${timeStr} ${FC.Gray}|| ${FC.MaterialRedstone}${rightScore}`;
     }
 }
