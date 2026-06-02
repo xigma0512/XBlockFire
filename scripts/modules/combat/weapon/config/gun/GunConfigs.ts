a4d0a27fbab7f2f96893257a627a958bbda426a5
xigma0512 <xig1517@gmail.com>
2026-06-03 02:04:47 +0800

    refactor: 微調平衡性、優化表單架構

--- scripts/modules/combat/weapon/config/gun/GunConfigs.ts
+++ scripts/modules/combat/weapon/config/gun/GunConfigs.ts
@@ -4,100 +4,19 @@ import { GunConfigType } from './GunConfigType';
 
 const defaultItem = (nametag: string) => ({
     nametag,
-    lore: ["I'M A GUN!!!"],
+    lore: [],
     keepOnDeath: true,
     lockMode: ItemLockMode.slot,
 });
 
-export const GUN_CONFIGS = {
-    ak47: {
-        id: 'ak47',
-        itemTypeId: 'xblockfire:ak47',
-        item: defaultItem('AK47'),
-        weight: 0.1,
-        gunTypeId: GunTypeEnum.AK47,
-        raiseTime: 8,
-        magazine: { ammo: 25, count: 5 },
-        fire: {
-            mode: FireModeEnum['Fully-Auto'],
-            releaseToFire: false,
-            bulletSpread: 1,
-            rate: 3,
-            sound: 'xblockfire.ak47_fire',
-        },
-        recoil: {
-            scope: { level: 0.02, duration: 0.2 },
-            hipfire: { level: 0.06, duration: 0.2 },
-        },
-        damage: {
-            near: { head: 12, body: 10, legs: 8 },
-            medium: { head: 11, body: 9, legs: 7 },
-            far: { head: 9, body: 8, legs: 6 },
-        },
-        reload: { time: 45, sound: 'xblockfire.ak47_reload' },
-        offset: { scope: 0.001, hipfire: 0.035, movement: 0.04 },
-    },
-    awp: {
-        id: 'awp',
-        itemTypeId: 'minecraft:spyglass',
-        item: defaultItem('AWP'),
-        weight: 0.08,
-        gunTypeId: GunTypeEnum.AWP,
-        raiseTime: 15,
-        magazine: { ammo: 5, count: 5 },
-        fire: {
-            mode: FireModeEnum['Semi-Auto'],
-            releaseToFire: true,
-            bulletSpread: 1,
-            rate: 20,
-            sound: 'xblockfire.awp_fire',
-        },
-        recoil: {
-            scope: { level: 0.06, duration: 0.2 },
-            hipfire: { level: 0.1, duration: 0.2 },
-        },
-        damage: {
-            near: { head: 100, body: 58, legs: 45 },
-            medium: { head: 100, body: 58, legs: 43 },
-            far: { head: 100, body: 58, legs: 40 },
-        },
-        reload: { time: 60, sound: 'xblockfire.awp_reload' },
-        offset: { scope: 0, hipfire: 0.008, movement: 0.5 },
-    },
-    deagle: {
-        id: 'deagle',
-        itemTypeId: 'xblockfire:deagle',
-        item: defaultItem('Deagle'),
-        weight: 0.12,
-        gunTypeId: GunTypeEnum.Deagle,
-        raiseTime: 5,
-        magazine: { ammo: 7, count: 5 },
-        fire: {
-            mode: FireModeEnum['Semi-Auto'],
-            releaseToFire: false,
-            bulletSpread: 1,
-            rate: 5,
-            sound: 'xblockfire.deagle_fire',
-        },
-        recoil: {
-            scope: { level: 0.02, duration: 0.2 },
-            hipfire: { level: 0.06, duration: 0.2 },
-        },
-        damage: {
-            near: { head: 16, body: 13, legs: 10 },
-            medium: { head: 14, body: 11, legs: 9 },
-            far: { head: 11, body: 9, legs: 7 },
-        },
-        reload: { time: 36, sound: 'xblockfire.deagle_reload' },
-        offset: { scope: 0.001, hipfire: 0.008, movement: 0.012 },
-    },
+const pistol = {
     glock17: {
         id: 'glock17',
         itemTypeId: 'xblockfire:glock17',
         item: defaultItem('Glock17'),
-        weight: 0.125,
+        weight: 0.12,
         gunTypeId: GunTypeEnum.Glock17,
-        raiseTime: 4,
+        raiseTime: 7,
         magazine: { ammo: 12, count: 5 },
         fire: {
             mode: FireModeEnum['Semi-Auto'],
@@ -118,13 +37,70 @@ export const GUN_CONFIGS = {
         reload: { time: 25, sound: 'xblockfire.glock17_reload' },
         offset: { scope: 0, hipfire: 0.005, movement: 0.004 },
     },
+    deagle: {
+        id: 'deagle',
+        itemTypeId: 'xblockfire:deagle',
+        item: defaultItem('Deagle'),
+        weight: 0.12,
+        gunTypeId: GunTypeEnum.Deagle,
+        raiseTime: 8,
+        magazine: { ammo: 7, count: 5 },
+        fire: {
+            mode: FireModeEnum['Semi-Auto'],
+            releaseToFire: false,
+            bulletSpread: 1,
+            rate: 5,
+            sound: 'xblockfire.deagle_fire',
+        },
+        recoil: {
+            scope: { level: 0.02, duration: 0.2 },
+            hipfire: { level: 0.06, duration: 0.2 },
+        },
+        damage: {
+            near: { head: 16, body: 13, legs: 10 },
+            medium: { head: 14, body: 11, legs: 9 },
+            far: { head: 11, body: 9, legs: 7 },
+        },
+        reload: { time: 36, sound: 'xblockfire.deagle_reload' },
+        offset: { scope: 0.001, hipfire: 0.008, movement: 0.012 },
+    },
+};
+
+const assault = {
+    ak47: {
+        id: 'ak47',
+        itemTypeId: 'xblockfire:ak47',
+        item: defaultItem('AK47'),
+        weight: 0.1,
+        gunTypeId: GunTypeEnum.AK47,
+        raiseTime: 15,
+        magazine: { ammo: 25, count: 5 },
+        fire: {
+            mode: FireModeEnum['Fully-Auto'],
+            releaseToFire: false,
+            bulletSpread: 1,
+            rate: 3,
+            sound: 'xblockfire.ak47_fire',
+        },
+        recoil: {
+            scope: { level: 0.02, duration: 0.2 },
+            hipfire: { level: 0.06, duration: 0.2 },
+        },
+        damage: {
+            near: { head: 12, body: 10, legs: 8 },
+            medium: { head: 11, body: 9, legs: 7 },
+            far: { head: 9, body: 8, legs: 6 },
+        },
+        reload: { time: 45, sound: 'xblockfire.ak47_reload' },
+        offset: { scope: 0.001, hipfire: 0.035, movement: 0.04 },
+    },
     m4a4: {
         id: 'm4a4',
         itemTypeId: 'xblockfire:m4a4',
         item: defaultItem('M4A4'),
-        weight: 0.105,
+        weight: 0.1,
         gunTypeId: GunTypeEnum.M4A4,
-        raiseTime: 8,
+        raiseTime: 15,
         magazine: { ammo: 30, count: 5 },
         fire: {
             mode: FireModeEnum['Fully-Auto'],
@@ -145,13 +121,46 @@ export const GUN_CONFIGS = {
         reload: { time: 40, sound: 'xblockfire.m4a4_reload' },
         offset: { scope: 0.001, hipfire: 0.025, movement: 0.025 },
     },
+};
+
+const sniper = {
+    awp: {
+        id: 'awp',
+        itemTypeId: 'minecraft:spyglass',
+        item: defaultItem('AWP'),
+        weight: 0.095,
+        gunTypeId: GunTypeEnum.AWP,
+        raiseTime: 25,
+        magazine: { ammo: 5, count: 5 },
+        fire: {
+            mode: FireModeEnum['Semi-Auto'],
+            releaseToFire: true,
+            bulletSpread: 1,
+            rate: 20,
+            sound: 'xblockfire.awp_fire',
+        },
+        recoil: {
+            scope: { level: 0.06, duration: 0.2 },
+            hipfire: { level: 0.1, duration: 0.2 },
+        },
+        damage: {
+            near: { head: 100, body: 58, legs: 45 },
+            medium: { head: 100, body: 58, legs: 43 },
+            far: { head: 100, body: 58, legs: 40 },
+        },
+        reload: { time: 60, sound: 'xblockfire.awp_reload' },
+        offset: { scope: 0, hipfire: 0.008, movement: 0.5 },
+    },
+};
+
+const machine_gun = {
     p90: {
         id: 'p90',
         itemTypeId: 'xblockfire:p90',
         item: defaultItem('P90'),
-        weight: 0.115,
+        weight: 0.11,
         gunTypeId: GunTypeEnum.P90,
-        raiseTime: 7,
+        raiseTime: 12,
         magazine: { ammo: 50, count: 5 },
         fire: {
             mode: FireModeEnum['Fully-Auto'],
@@ -172,19 +181,22 @@ export const GUN_CONFIGS = {
         reload: { time: 50, sound: 'xblockfire.p90_reload' },
         offset: { scope: 0.008, hipfire: 0.02, movement: 0.008 },
     },
+};
+
+const shotgun = {
     sg200: {
         id: 'sg200',
         itemTypeId: 'xblockfire:sg200',
         item: defaultItem('SG200'),
-        weight: 0.095,
+        weight: 0.09,
         gunTypeId: GunTypeEnum.SG200,
-        raiseTime: 10,
+        raiseTime: 20,
         magazine: { ammo: 6, count: 5 },
         fire: {
             mode: FireModeEnum['Semi-Auto'],
             releaseToFire: false,
             bulletSpread: 8,
-            rate: 14,
+            rate: 20,
             sound: 'xblockfire.sg200_fire',
         },
         recoil: {
@@ -199,4 +211,12 @@ export const GUN_CONFIGS = {
         reload: { time: 40, sound: 'xblockfire.sg200_reload' },
         offset: { scope: 0.05, hipfire: 0.09, movement: 0.06 },
     },
-} as const satisfies Record<string, GunConfigType>;
+};
+
+export const GUN_CONFIGS = {
+    ...pistol,
+    ...assault,
+    ...sniper,
+    ...machine_gun,
+    ...shotgun,
+} as Record<string, GunConfigType>;
