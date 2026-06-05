3c1891d9feb8033eae86171e6a9c9a4d453e0607
xigma0512 <xig1517@gmail.com>
2026-06-05 16:00:02 +0800

    feat: 實作死鬥模式 Idle 與 PreStart 流程

--- scripts/modules/core/gamephase/deathmatch/Idle.ts
+++ scripts/modules/core/gamephase/deathmatch/Idle.ts
@@ -1,13 +1,93 @@
+import { MemberManager } from '../../../player/MemberManager';
+import { LoadoutManager } from '../../LoadoutManager';
+import { PhaseManager } from '../PhaseManager';
+import { WaitingView as WaitingHud } from '../../../../ui/hud/views/WaitingView';
+import { DeathmatchPreStartPhase } from './PreStart';
 import { PhaseIdentity } from '../PhaseIdentity';
-import { InGameHud } from '../../../../ui/InGameHud';
+import { TeamEnum } from '../../../player/TeamEnum';
+import { Language as L } from '../../../../utils/Language';
+import { reset_variables, set_variable } from '../../../../utils/Variable';
+import { Sound } from '../../../../ui/media/Sound';
+import { Config } from '../../../../settings/config';
+
+const COUNTDOWN_TIME = 20 * 20;
 
 export class DeathmatchIdlePhase implements IPhaseHandler {
     readonly phaseTag = 100;
     readonly phaseId = PhaseIdentity.Deathmatch.Idle;
-    readonly hud!: InGameHud;
-    readonly currentTick = -1;
+    readonly hud: WaitingHud;
 
-    on_entry() {}
-    on_running() {}
-    on_exit() {}
+    private _currentTick = COUNTDOWN_TIME;
+    get currentTick() {
+        return this._currentTick;
+    }
+
+    constructor() {
+        this.hud = new WaitingHud();
+    }
+
+    on_entry() {
+        this._currentTick = COUNTDOWN_TIME;
+    }
+
+    on_running() {
+        const members = MemberManager.getPlayers();
+        const countdownWasIdle = this.currentTick === COUNTDOWN_TIME;
+
+        if (members.length >= Config.game.AUTO_START_MIN_PLAYER) {
+            if (countdownWasIdle) Sound.play('WAITING_COUNTDOWN_START', members);
+            this._currentTick--;
+        }
+
+        if (this.currentTick !== COUNTDOWN_TIME && members.length < Config.game.AUTO_START_MIN_PLAYER) {
+            this._currentTick = COUNTDOWN_TIME;
+            Sound.play('WAITING_COUNTDOWN_CANCEL', members);
+        }
+
+        this.hud.update();
+        if (this.currentTick <= 0) PhaseManager.updatePhase(new DeathmatchPreStartPhase());
+    }
+
+    on_exit() {
+        randomTeam();
+        initializePlayers();
+        initializeVariables();
+    }
+}
+
+function randomTeam() {
+    const players = MemberManager.getPlayers();
+    const shuffledPlayers = [...players].sort(() => 0.5 - Math.random());
+
+    let attackTeamCount = 0;
+    let defenderTeamCount = 0;
+    for (const player of shuffledPlayers) {
+        if (attackTeamCount <= defenderTeamCount) {
+            MemberManager.setPlayerTeam(player, TeamEnum.Attacker);
+            attackTeamCount++;
+            player.sendMessage(L.translate('game.assigned.attacker'));
+        } else {
+            MemberManager.setPlayerTeam(player, TeamEnum.Defender);
+            defenderTeamCount++;
+            player.sendMessage(L.translate('game.assigned.defender'));
+        }
+    }
+}
+
+function initializePlayers() {
+    for (const player of MemberManager.getPlayers()) {
+        LoadoutManager.initializePlayer(player);
+    }
+}
+
+function initializeVariables() {
+    reset_variables();
+    set_variable('attacker_score', 0);
+    set_variable('defender_score', 0);
+    set_variable('winner', TeamEnum.Spectator);
+    set_variable('deathmatch_sudden_death', false);
+    for (const player of MemberManager.getPlayers()) {
+        set_variable(`${player.name}.kills`, 0);
+        set_variable(`${player.name}.deaths`, 0);
+    }
 }
--- scripts/modules/core/gamephase/deathmatch/PreStart.ts
+++ scripts/modules/core/gamephase/deathmatch/PreStart.ts
@@ -1,7 +1,13 @@
-import { PhaseIdentity } from '../PhaseIdentity';
+import { GameMode, InputPermissionCategory } from '@minecraft/server';
+import { MemberManager } from '../../../player/MemberManager';
+import { TeamEnum } from '../../../player/TeamEnum';
 import { PhaseManager } from '../PhaseManager';
 import { DeathmatchActionPhase } from './Action';
+import { PhaseIdentity } from '../PhaseIdentity';
 import { InGameHud } from '../../../../ui/InGameHud';
+import { set_entity_dynamic_property, set_entity_native_property } from '../../../../utils/Property';
+import { DeathmatchSpawn } from '../../deathmatch/DeathmatchSpawn';
+import { DeathmatchLoadout } from '../../deathmatch/DeathmatchLoadout';
 
 export class DeathmatchPreStartPhase implements IPhaseHandler {
     readonly phaseTag = 101;
@@ -9,7 +15,25 @@ export class DeathmatchPreStartPhase implements IPhaseHandler {
     readonly hud!: InGameHud;
     readonly currentTick = -1;
 
-    on_entry() {}
+    on_entry() {
+        for (const player of MemberManager.getPlayers()) {
+            const team = MemberManager.getPlayerTeam(player);
+
+            if (team === TeamEnum.Spectator) {
+                player.setGameMode(GameMode.Spectator);
+                continue;
+            }
+
+            player.teleport(DeathmatchSpawn.randomSpawn(team));
+            player.setGameMode(GameMode.Adventure);
+            player.inputPermissions.setPermissionCategory(InputPermissionCategory.LateralMovement, true);
+            set_entity_dynamic_property(player, 'player:is_alive', true);
+            set_entity_native_property(player, 'player:can_use_item', true);
+            player.addEffect('regeneration', 40, { amplifier: 255, showParticles: false });
+            player.addEffect('saturation', 20, { amplifier: 5, showParticles: false });
+            DeathmatchLoadout.apply(player);
+        }
+    }
 
     on_running() {
         PhaseManager.updatePhase(new DeathmatchActionPhase());
